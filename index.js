7b9b3796f998974521ad9e3f73059aaa11e61305
Alex O Bunnyshell <eaasmigrations@bunnyshell.com>
2024-04-25 20:17:15 +0300

    initial version

---
+++ index.js
@@ -0,0 +1,15 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+    console.log('Hello world! This is a WebSocket Mirror server');
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+    ws.send(data);
+  });
+
+  ws.send('something');
+});
