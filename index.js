7c401d31b1263029760ae673731043fd45a133dd
Alex O Bunnyshell <eaasmigrations@bunnyshell.com>
2024-04-26 16:23:21 +0300

    add http server

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8081 });
 
-wss.on('connection', function connection(ws) {
+// read the port from the environment variables
+// if the port is not set, use 8080 as default
+
+const webServerPort = process.env.WEB_PORT || 8080;
+const socketServerPort = process.env.SOCKET_PORT || 8081;
+
+
+const webServer = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  });
+  
+  webServer.listen( webServerPort , () => {
+    console.log(`HTTP server running at port ${webServerPort}`);
+  });
+
+const socketServer = new WebSocketServer({ port: socketServerPort });
+
+socketServer.on('connection', function connection(ws) {
     console.log('Hello world! This is a WebSocket Mirror server');
   ws.on('error', console.error);
 
